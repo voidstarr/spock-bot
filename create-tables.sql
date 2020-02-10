9bfd8cf029c52c1f34e20529317ab3e2f402c17c
voidstarr <voidstarr@users.noreply.github.com>
2020-02-10 19:12:15 +0000

    unique discord ids in db

--- create-tables.sql
+++ create-tables.sql
@@ -2,7 +2,7 @@ CREATE DATABASE IF NOT EXISTS spock_bot;
 
 CREATE TABLE IF NOT EXISTS spock_bot.channel_messages (
     db_id INT NOT NULL AUTO_INCREMENT,
-    id BIGINT UNSIGNED,
+    id BIGINT UNSIGNED UNIQUE,
     server BIGINT UNSIGNED,
     channel BIGINT UNSIGNED,
     author BIGINT UNSIGNED,
@@ -15,7 +15,7 @@ COLLATE utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS spock_bot.direct_messages (
     db_id INT NOT NULL AUTO_INCREMENT,
-    id BIGINT UNSIGNED,
+    id BIGINT UNSIGNED UNIQUE,
     channel BIGINT UNSIGNED,
     author BIGINT UNSIGNED,
     created_at DATETIME,
@@ -27,7 +27,7 @@ COLLATE utf8mb4_bin;
 
 CREATE TABLE IF NOT EXISTS spock_bot.users (
     db_id INT NOT NULL AUTO_INCREMENT,
-    id BIGINT UNSIGNED,
+    id BIGINT UNSIGNED UNIQUE,
     username VARCHAR(32),
     discriminator VARCHAR(32),
     PRIMARY KEY ( db_id )
